3c87362252a456470faae0dbbe61f58a4805cb7c
Mateusz Lubowiecki <matlubowiecki@gmail.com>
2023-12-21 00:00:12 +0100

    Auction Edit @

--- src/main/java/xloproject001/xloProject001/model/Auction.java
+++ src/main/java/xloproject001/xloProject001/model/Auction.java
@@ -1,57 +1,26 @@
 package xloproject001.xloProject001.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "auctions")
 public class Auction {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    private String name;
+    private String title;
     private String description;
     private int price;
+    private String imageurl;
 
-    public Auction() {
-    }
-
-    public Auction(int id, String name, String description, int price) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.price = price;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
 }
