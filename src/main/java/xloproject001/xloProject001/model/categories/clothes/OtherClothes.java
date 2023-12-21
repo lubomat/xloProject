5b80710d3766f3e36d2414eab4dbde2030ffac86
Mateusz Lubowiecki <matlubowiecki@gmail.com>
2023-12-21 12:17:19 +0100

    @Getter @Setter

--- src/main/java/xloproject001/xloProject001/model/categories/clothes/OtherClothes.java
+++ src/main/java/xloproject001/xloProject001/model/categories/clothes/OtherClothes.java
@@ -1,7 +1,10 @@
 package xloproject001.xloProject001.model.categories.clothes;
 
+import lombok.Getter;
+import lombok.Setter;
 import xloproject001.xloProject001.model.categories.Clothes;
 
-
+@Getter
+@Setter
 public class OtherClothes extends Clothes {
 }
--- src/main/java/xloproject001/xloProject001/model/categories/electronics/OtherElectronic.java
+++ src/main/java/xloproject001/xloProject001/model/categories/electronics/OtherElectronic.java
@@ -1,6 +1,10 @@
 package xloproject001.xloProject001.model.categories.electronics;
 
+import lombok.Getter;
+import lombok.Setter;
 import xloproject001.xloProject001.model.categories.Electronic;
 
+@Getter
+@Setter
 public class OtherElectronic extends Electronic {
 }
