fc6c186bb33d5ab6d5801cec135c7711ead2bbc7
Mateusz Lubowiecki <matlubowiecki@gmail.com>
2024-01-06 15:35:43 +0100

    VehicleRepository fixed

--- src/main/java/xloproject001/xloProject001/model/VehicleRepository.java
+++ src/main/java/xloproject001/xloProject001/model/VehicleRepository.java
@@ -2,12 +2,10 @@ package xloproject001.xloProject001.model;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
-@Repository
 public interface VehicleRepository {
 
     Vehicle save(Vehicle entity);
