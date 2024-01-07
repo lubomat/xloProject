d13258b588e00bde08615c1c82c138e4807091f6
Mateusz Lubowiecki <matlubowiecki@gmail.com>
2024-01-07 17:40:15 +0100

    edit Color @Entity

--- src/main/java/xloproject001/xloProject001/model/Color.java
+++ src/main/java/xloproject001/xloProject001/model/Color.java
@@ -1,11 +1,13 @@
 package xloproject001.xloProject001.model;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
 @Getter
 @Setter
 @Table(name = "colors")
